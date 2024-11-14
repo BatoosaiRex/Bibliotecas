7cd50bb6b2114f40e9c24eeb8655a17d1254d606
Broldan98 <roldanbe98@gmail.com>
2024-11-14 16:49:13 -0500

    ultimos cambios front

--- server.js
+++ server.js
@@ -1,59 +1,59 @@
-const express = require("express"); // Framework de servidor web
-const mysql = require("mysql2"); // Cliente MySQL para Node.js
-const cors = require("cors"); // Middleware para permitir peticiones desde otros dominios
-const app = express(); // Inicializar la aplicación Express
+const express = require("express"); 
+const mysql = require("mysql2"); 
+const cors = require("cors"); 
+const app = express(); 
 
-// Middleware para parsear el cuerpo de las peticiones como JSON
+
 app.use(express.json());
-// Habilitar CORS para permitir solicitudes desde dominios externos
+
 app.use(cors());
 
-// Configuración de la base de datos MySQL
+
 const db = mysql.createConnection({
-    host: "localhost", // Host donde se encuentra la base de datos
-    port: 3306, // Puerto de MySQL (por defecto 3306)
-    user: "root", // Usuario de la base de datos
-    database: "biblioteca", // Nombre de la base de datos
-    password: "nomelase",
+    host: "localhost", 
+    port: 3306, 
+    user: "root", 
+    database: "biblioteca", 
+   
 });
 
-// Conexión a la base de datos MySQL
+
 db.connect((err) => {
     if (err) {
-        // Si ocurre un error en la conexión
+        
         console.error("Error de conexión a la base de datos:", err);
         return;
     }
-    console.log("Conectado a la base de datos"); // Si la conexión es exitosa
+    console.log("Conectado a la base de datos"); 
 });
 
-// Rutas para manejar usuarios (registro y login)
 
-// Registrar un nuevo usuario
+
+
 app.post("/register", (req, res) => {
-    const { username, password } = req.body; // Obtener los datos del cuerpo de la petición
-    const query = "SELECT * FROM users WHERE username = ?"; // Consultar si el usuario ya existe
+    const { username, password } = req.body; 
+    const query = "SELECT * FROM users WHERE username = ?"; 
     db.query(query, [username], (err, result) => {
         if (err) {
             // Si ocurre un error en la consulta
             return res.status(500).json({ message: "Error de base de datos" });
         }
         if (result.length > 0) {
-            // Si el usuario ya existe en la base de datos
+            
             return res.status(400).json({ message: "El usuario ya existe" });
         }
 
-        // Si el usuario no existe, insertarlo en la base de datos
+        
         const insertQuery =
             "INSERT INTO users (username, password) VALUES (?, ?)";
         db.query(insertQuery, [username, password], (err, result) => {
             if (err) {
-                // Si ocurre un error al insertar el nuevo usuario
+                
                 return res
                     .status(500)
                     .json({ message: "Error de base de datos" });
             }
-            // Responder con éxito si el usuario fue registrado
+            
             res.status(201).json({
                 message: "Usuario registrado exitosamente",
             });
@@ -63,18 +63,18 @@ app.post("/register", (req, res) => {
 
 // Iniciar sesión
 app.post("/login", (req, res) => {
-    const { username, password } = req.body; // Obtener los datos de la solicitud
-    const query = "SELECT * FROM users WHERE username = ? AND password = ?"; // Consultar por el usuario y la contraseña
+    const { username, password } = req.body; 
+    const query = "SELECT * FROM users WHERE username = ? AND password = ?"; 
     db.query(query, [username, password], (err, result) => {
         if (err) {
             // Si ocurre un error en la consulta
             return res.status(500).json({ message: "Error de base de datos" });
         }
         if (result.length > 0) {
-            // Si se encuentra un usuario con las credenciales correctas
+            
             res.status(200).json({ message: "Login exitoso", user: result[0] });
         } else {
-            // Si no se encuentra el usuario o la contraseña es incorrecta
+            
             res.status(401).json({
                 message: "Usuario o contraseña incorrectos",
             });
@@ -82,84 +82,84 @@ app.post("/login", (req, res) => {
     });
 });
 
-// Rutas para manejar libros
 
-// Obtener todos los libros
+
+
 app.get("/books", (req, res) => {
-    const query = "SELECT * FROM books"; // Consulta para obtener todos los libros
+    const query = "SELECT * FROM books"; 
     db.query(query, (err, result) => {
         if (err) {
-            // Si ocurre un error en la consulta
+            
             return res.status(500).json({ message: "Error de base de datos" });
         }
-        res.status(200).json(result); // Retornar todos los libros en formato JSON
+        res.status(200).json(result); 
     });
 });
 
-// Agregar un nuevo libro
+
 app.post("/addBook", (req, res) => {
-    const { title, author, description, image, userId } = req.body; // Obtener los datos del libro a agregar
+    const { title, author, description, image, userId } = req.body; 
     const query =
-        "INSERT INTO books (title, author, description, image, user_id) VALUES (?, ?, ?, ?, ?)"; // Consulta para agregar un libro
+        "INSERT INTO books (title, author, description, image, user_id) VALUES (?, ?, ?, ?, ?)"; 
     db.query(
         query,
         [title, author, description, image, userId],
         (err, result) => {
             if (err) {
-                // Si ocurre un error al agregar el libro
+                
                 return res
                     .status(500)
                     .json({ message: "Error de base de datos", error: err });
                 console.log(err);
             }
-            res.status(201).json({ message: "Libro agregado exitosamente" }); // Retornar mensaje de éxito
+            res.status(201).json({ message: "Libro agregado exitosamente" }); 
         }
     );
 });
 
-// Agregar o actualizar la calificación de un libro
+
 app.post("/rateBook", (req, res) => {
-    const { bookId, userId, rating } = req.body; // Obtener los datos de la calificación
-    const query = "SELECT * FROM ratings WHERE book_id = ? AND user_id = ?"; // Consultar si ya existe una calificación del libro por el usuario
+    const { bookId, userId, rating } = req.body; 
+    const query = "SELECT * FROM ratings WHERE book_id = ? AND user_id = ?"; 
     db.query(query, [bookId, userId], (err, result) => {
         if (err) {
             // Si ocurre un error en la consulta
             return res.status(500).json({ message: "Error de base de datos" });
         }
         if (result.length > 0) {
-            // Si ya existe una calificación, se actualiza
+            
             const updateQuery =
                 "UPDATE ratings SET rating = ? WHERE book_id = ? AND user_id = ?";
             db.query(updateQuery, [rating, bookId, userId], (err, result) => {
                 if (err) {
-                    // Si ocurre un error al actualizar la calificación
+                    
                     return res.status(500).json({
                         message: "Error al actualizar la calificación",
                     });
                 }
-                res.status(200).json({ message: "Calificación actualizada" }); // Retornar mensaje de éxito
+                res.status(200).json({ message: "Calificación actualizada" }); 
             });
         } else {
-            // Si no existe una calificación, se agrega una nueva
+            
             const insertQuery =
                 "INSERT INTO ratings (book_id, user_id, rating) VALUES (?, ?, ?)";
             db.query(insertQuery, [bookId, userId, rating], (err, result) => {
                 if (err) {
-                    // Si ocurre un error al insertar la calificación
+                    
                     return res
                         .status(500)
                         .json({ message: "Error al agregar la calificación" });
                 }
-                res.status(201).json({ message: "Calificación agregada" }); // Retornar mensaje de éxito
+                res.status(201).json({ message: "Calificación agregada" }); 
             });
         }
     });
 });
 
-// Iniciar el servidor
-const PORT = process.env.PORT || 3000; // Usar el puerto del entorno o 3000 por defecto
+
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`); // Confirmar que el servidor está corriendo
+    console.log(`Servidor corriendo en puerto ${PORT}`); 
 });
 
 app.get("/books/:id", (req, res) => {
@@ -172,7 +172,7 @@ app.get("/books/:id", (req, res) => {
             if (error) throw error;
 
             if (bookResults.length > 0) {
-                // Consultar las valoraciones del libro
+                
                 db.query(
                     "SELECT * FROM ratings WHERE book_id = ?",
                     [bookId],
